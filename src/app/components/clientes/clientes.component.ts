6fb9ca4b585f90174424d06468ece18454971093
renatomcc <renattocasttro@gmail.com>
2023-03-02 23:42:51 -0300

    clients component improved with props

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+interface Customer {
+  image: string;
+  alt: string;
+}
+
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.sass']
+  styleUrls: ['./clientes.component.sass'],
 })
 export class ClientesComponent {
-
+  customers: Customer[] = [
+    {
+      image: '../../../assets/c1.png',
+      alt: 'Sambaíba',
+    },
+    {
+      image: '../../../assets/c2.png',
+      alt: 'BRT Sorocaba',
+    },
+    {
+      image: '../../../assets/c3.png',
+      alt: 'Mobi-PE',
+    },
+    {
+      image: '../../../assets/c4.png',
+      alt: 'Mobi-Brasil',
+    },
+    {
+      image: '../../../assets/c5.png',
+      alt: 'Nova Mobi Pernambuco',
+    },
+  ];
 }
