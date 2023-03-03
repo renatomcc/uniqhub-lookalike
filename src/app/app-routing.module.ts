f379467e2bf4d05277059b1c2ef80383d2ec7ea6
renatomcc <renattocasttro@gmail.com>
2023-03-03 04:41:40 -0300

    header image fixed and images draggable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuemSomosComponent } from './components/quem-somos/quem-somos.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { SolucoesComponent } from './components/solucoes/solucoes.component';
+import { PerguntasFrequentesComponent } from './components/perguntas-frequentes/perguntas-frequentes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: QuemSomosComponent },
+  { path: 'quem-somos', component: QuemSomosComponent },
+  { path: 'solucoes', component: SolucoesComponent },
+  { path: 'perguntas-frequentes', component: PerguntasFrequentesComponent },
+  { path: 'nossos-clientes', component: ClientesComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes,{scrollPositionRestoration: 'enabled'})],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { QuemSomosComponent } from './components/quem-somos/quem-somos.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SolucoesComponent } from './components/solucoes/solucoes.component';
+import { PerguntasFrequentesComponent } from './components/perguntas-frequentes/perguntas-frequentes.component';
+import { ScrollButtonComponent } from './components/scroll-button/scroll-button.component';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     NavbarComponent,
     QuemSomosComponent,
     ClientesComponent,
+    SolucoesComponent,
+    PerguntasFrequentesComponent,
+    ScrollButtonComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, SlickCarouselModule, FontAwesomeModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
