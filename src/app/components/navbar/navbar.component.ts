3a7dd5e4cc57d13a32b4ad209b4e78b2bef36c80
renatomcc <renattocasttro@gmail.com>
2023-03-03 03:40:13 -0300

    responsive navbar added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import {faWindowClose, faFolderOpen} from '@fortawesome/free-regular-svg-icons'
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.sass'],
 })
 export class NavbarComponent {
+  mobile = true;
+  menuClose = faWindowClose;
+  menuOpen = faFolderOpen;
+  
+  handleMenu(): void {
+    this.mobile = !this.mobile;
+  }
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.mobile = false;
+      }
+    });
+  }
 }
